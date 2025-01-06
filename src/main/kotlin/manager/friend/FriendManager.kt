89ddd36a2db457754cd2e3c8df717542c815ef27
admin <xxx@xx.xom>
2025-01-06 11:09:41 +0800

    Initial commit

--- src/main/kotlin/manager/friend/FriendManager.kt
+++ src/main/kotlin/manager/friend/FriendManager.kt
@@ -9,42 +9,39 @@ object FriendManager {
 
     }
 
-    /**
-     * @return 0: 不是好友; 1: 左边是第一层键; -1: 右边是第一层键
-     */
-    fun checkFriend(lhs: Long, rhs: Long): Int {
-        if (lhs <= 1000 || rhs <= 1000) return 0
-        if (lhs == rhs) return 0
+    fun checkFriend(lhs: Long, rhs: Long): Boolean {
+        if (lhs <= 1000 || rhs <= 1000) return false
+        if (lhs == rhs) return false
 
         friendMap[lhs]?.let {
             it[rhs]?.let {
-                return if (it.startTime > 0) 1
-                else 0
+                return it.startTime > 0
             }
         }
 
         friendMap[rhs]?.let {
             it[lhs]?.let {
-                return if (it.startTime > 0) -1
-                else 0
+                return it.startTime > 0
             }
         }
 
-        return 0
+        return false
     }
 
     fun addFriend(lhs: Long, rhs: Long): Int {
-        checkFriend(lhs, rhs).takeUnless { it == 0 }?.let {
+        checkFriend(lhs, rhs).takeIf { it }?.let {
             return 1
         }
 
         // TODO: 加好友限制检查
 
-        if (!friendMap.containsKey(lhs)) {
+        if (!friendMap.containsKey(lhs))
             friendMap[lhs] = hashMapOf<Long, FriendInfo>()
-        }
-
         friendMap[lhs]!![rhs] = FriendInfo(lhs, rhs, System.currentTimeMillis())
+
+        if (!friendMap.containsKey(rhs))
+            friendMap[rhs] = hashMapOf<Long, FriendInfo>()
+        friendMap[rhs]!![lhs] = FriendInfo(rhs, lhs, System.currentTimeMillis())
 
         // TODO: 同步数据库
 
@@ -52,26 +49,13 @@ object FriendManager {
     }
 
     fun removeFriend(lhs: Long, rhs: Long): Int {
-        when (checkFriend(lhs, rhs)) {
-            0 -> return 0
-            1 -> {
-                friendMap[lhs]?.let {
-                    if (it.containsKey(rhs)) {
-                        it.remove(rhs)
-                        return 0
-                    }
-                }
-            }
-            -1 -> {
-                friendMap[rhs]?.let {
-                    if (it.containsKey(lhs)) {
-                        it.remove(lhs)
-                        return 0
-                    }
-                }
-            }
+        checkFriend(lhs, rhs).takeUnless { it }?.let {
+            return 1
         }
 
-        return 2
+        friendMap[lhs]?.remove(rhs)
+        friendMap[rhs]?.remove(lhs)
+
+        return 0
     }
 }
