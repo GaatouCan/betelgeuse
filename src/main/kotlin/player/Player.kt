f985365c87ef3d328a942e27da4be3da7d8b1322
admin <xxx@xx.xom>
2025-01-06 14:37:45 +0800

    Initial commit

--- src/main/kotlin/player/Player.kt
+++ src/main/kotlin/player/Player.kt
@@ -3,10 +3,12 @@ package org.example.player
 import io.netty.channel.ChannelHandlerContext
 import org.apache.logging.log4j.LogManager
 import org.example.base.net.AttributeKeys
+import kotlin.reflect.KClass
 
 class Player(val context: ChannelHandlerContext) {
 
     private val logger = LogManager.getLogger(Player::class.java)
+    val componentMap = hashMapOf<KClass<out BaseComponent>, BaseComponent>()
 
     fun getPlayerID(): Long {
         return context.channel().attr(AttributeKeys.PLAYER_ID).get() ?: 0
@@ -18,5 +20,11 @@ class Player(val context: ChannelHandlerContext) {
 
     fun onLogout() {
         logger.info("Player[${getPlayerID()}] has logged out")
+    }
+
+    inline fun <reified T: BaseComponent> getComponent(componentClass: KClass<T>) : T? {
+        return componentMap[componentClass]?.let {
+            it as? T
+        }
     }
 }
