8a2390a77b5176f123adcf64fa7e14a8bc9aa853
admin <xxx@xx.xom>
2025-01-06 09:48:15 +0800

    Initial commit

--- src/main/resources/gen-proto.bat
+++ src/main/resources/gen-proto.bat
@@ -4,14 +4,15 @@ cd ../
 set PROTOC=D:\library\install\protobuf\bin\protoc.exe
 set PROTO_DIR=.\resources\proto\
 
-
 for %%F in (%PROTO_DIR%\*) do (
     :: 检查文件扩展名是否为 .proto
     if /i "%%~xF"==".proto" (
         echo %%~nxF
     )
 )
-
+if NOT exist ".\java" (
+    mkdir .\java
+)
 call %PROTOC% --proto_path=%PROTO_DIR% --java_out=.\java --kotlin_out=.\kotlin %PROTO_DIR%*.proto
 
 pause
