464cab385c3532a5ca10d3e86a8a3dcfbff21ebb
admin <xxx@xx.xom>
2025-02-05 20:40:06 +0800

    Initial commit

--- src/main/kotlin/base/table/AvatarTable.kt
+++ src/main/kotlin/base/table/AvatarTable.kt
@@ -3,8 +3,8 @@ package org.example.base.table
 import org.jetbrains.exposed.sql.Table
 
 object AvatarTable : Table() {
-    var playerId = long("player_id")
-    var avatarIndex = integer("avatar_idx")
+    val playerId = long("player_id")
+    val avatarIndex = integer("avatar_idx")
     val isActivated = bool("is_activated").default(false)
     val isInUse = bool("is_in_use").default(false)
 
