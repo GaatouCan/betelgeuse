efcf6e98a29c2b41a10111ef5ee8acd385a6407f
admin <xxx@xx.xom>
2025-03-06 14:12:56 +0800

    Initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.21"
+    kotlin("plugin.serialization") version "1.9.0"
     application
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
@@ -17,33 +18,32 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
 
     // Protobuf
-    implementation("com.google.protobuf:protobuf-java:4.29.3")
-    implementation("com.google.protobuf:protobuf-java-util:4.29.3")
-    implementation("com.google.protobuf:protobuf-kotlin:4.29.3")
+    implementation("com.google.protobuf:protobuf-java:4.30.0")
+    implementation("com.google.protobuf:protobuf-java-util:4.30.0")
+    implementation("com.google.protobuf:protobuf-kotlin:4.30.0")
 
     // Netty
-    implementation("io.netty:netty-all:4.1.116.Final")
+    implementation("io.netty:netty-all:4.1.119.Final")
 
     // Log4j
     implementation("org.apache.logging.log4j:log4j-core:2.24.3")
 
     // Reflection
-    implementation("org.reflections:reflections:0.9.12")
+    implementation("org.reflections:reflections:0.10.2")
 
     // YAML
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.2")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.2")
+    implementation("org.yaml:snakeyaml:2.4")
 
     // JSON
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
 
     implementation("com.zaxxer:HikariCP:6.2.1")
 
-    implementation("org.jetbrains.exposed:exposed-core:0.59.0")
-    implementation("org.jetbrains.exposed:exposed-dao:0.59.0")
-    implementation("org.jetbrains.exposed:exposed-java-time:0.59.0")
+    implementation("org.jetbrains.exposed:exposed-core:0.60.0")
+    implementation("org.jetbrains.exposed:exposed-dao:0.60.0")
+    implementation("org.jetbrains.exposed:exposed-java-time:0.60.0")
 
-    runtimeOnly("org.jetbrains.exposed:exposed-jdbc:0.59.0")
+    runtimeOnly("org.jetbrains.exposed:exposed-jdbc:0.60.0")
 }
 
 sourceSets {
